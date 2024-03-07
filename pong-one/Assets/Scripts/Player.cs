9f17c409526cd6aeced5b5e530a657a347274018
Christian-Ashley <Christianashley86@gmail.com>
2024-03-06 22:35:27 -0600

    i dont know where i fucked this up but whatever

--- pong-one/Assets/Scripts/Player.cs
+++ pong-one/Assets/Scripts/Player.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float paddleSpeed = 15;
     public KeyCode upKey;
     public KeyCode downKey;
+
+void Update()
+{
+    // Move the paddle up if the upKey is pressed
+    if (Input.GetKey(upKey))
+    {
+        transform.Translate(Vector3.up * paddleSpeed * Time.deltaTime);
+    }
+
+    // Move the paddle down if the downKey is pressed
+    if (Input.GetKey(downKey))
+    {
+        transform.Translate(Vector3.down * paddleSpeed * Time.deltaTime);
+    }
+
+}
 }
